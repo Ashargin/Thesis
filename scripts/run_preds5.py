2e11908917fb232b91d90f8cc205a93c2cf6a995
Ashargin <loic.omnes@free.fr>
2024-01-12 13:30:26 +0100

    prepare scripts for ensemble computations

--- scripts/run_preds5.py
+++ scripts/run_preds5.py
@@ -8,13 +8,13 @@ from tensorflow import keras
 
 from src.predict import (
     divide_predict,
-    linearfold_predict,
+    ensemble_predict,
 )
 from src.models.loss import inv_exp_distance_to_cut_loss
 from src.utils import run_preds
 
 model = keras.models.load_model(
-    Path("resources/models/CNN1D_sequencewise"), compile=False
+    Path("resources/models/MLP_sequencewise"), compile=False
 )
 model.compile(
     optimizer="adam",
@@ -25,11 +25,11 @@ model.compile(
 
 run_preds(
     divide_predict,
-    Path("resources/divide_cnn_1000_lf_sequencewise_RETEST.csv"),
+    Path("resources/divide_mlp_1000_ens_sequencewise.csv"),
     in_filename="test_sequencewise",
     kwargs={
         "max_length": 1000,
         "cut_model": model,  # with motifs input format
-        "predict_fnc": linearfold_predict,
+        "predict_fnc": ensemble_predict,
     },
 )
