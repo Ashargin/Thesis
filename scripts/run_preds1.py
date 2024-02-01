91fea34d35103689d02d39d4d928e34766adf039
Ashargin <loic.omnes@free.fr>
2024-02-01 13:43:50 +0100

    prepare scripts for limited motifs computation

--- scripts/run_preds1.py
+++ scripts/run_preds1.py
@@ -14,7 +14,7 @@ from src.models.loss import inv_exp_distance_to_cut_loss
 from src.utils import run_preds
 
 model = keras.models.load_model(
-    Path("resources/models/CNN1D_sequencewise"), compile=False
+    Path("resources/models/CNN1D_sequencewise_0motifs"), compile=False
 )
 model.compile(
     optimizer="adam",
@@ -25,11 +25,12 @@ model.compile(
 
 run_preds(
     divide_predict,
-    Path("resources/divide_cnn_1000_mx_benchmarkLncrna.csv"),
-    in_filename="benchmark_lncRNAs",
+    Path("resources/divide_cnn0motifs_1000_mx_sequencewise.csv"),
+    in_filename="test_sequencewise",
     kwargs={
         "max_length": 1000,
         "cut_model": model,  # with motifs input format
         "predict_fnc": mxfold2_predict,
+        "max_motifs": 0,
     },
 )
