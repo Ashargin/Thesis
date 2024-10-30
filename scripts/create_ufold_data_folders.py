34e5aa3f5810ecff6ba7946bf7dbdc2e5224bf42
Ashargin <loic.omnes@free.fr>
2024-10-31 00:51:32 +0100

    only create ufold data folder for train_reduced

--- scripts/create_ufold_data_folders.py
+++ scripts/create_ufold_data_folders.py
@@ -9,18 +9,18 @@ from src.utils import struct_to_pairs
 
 path = Path(r"resources/data_structures")
 
-for f_in in os.listdir(path):
-    dirname, _ = os.path.splitext(f_in)
-    os.mkdir(path / dirname)
-    lines = open(path / f_in, "r").read().strip().split("\n")
-    names, seqs, structs = lines[::3], lines[1::3], lines[2::3]
-    names = [n.split("#Name: ")[1] for n in names]
-    for n, se, st in zip(names, seqs, structs):
-        n = n.replace("/", "~")  # to allow text file creation
-        pairs = struct_to_pairs(st)
-        with open(path / dirname / (n + ".bpseq"), "w") as f_out:
-            for i, p in enumerate(pairs):
-                nuc = se[i]
-                if nuc not in ["A", "U", "C", "G"]:
-                    nuc = np.random.choice(["A", "U", "C", "G"])
-                f_out.write(f"{i+1} {se[i]} {p}\n")
+f_in = "train_reduced.dbn"
+dirname, _ = os.path.splitext(f_in)
+os.mkdir(path / dirname)
+lines = open(path / f_in, "r").read().strip().split("\n")
+names, seqs, structs = lines[::3], lines[1::3], lines[2::3]
+names = [n.split("#Name: ")[1] for n in names]
+for n, se, st in zip(names, seqs, structs):
+    n = n.replace("/", "~")  # to allow text file creation
+    pairs = struct_to_pairs(st)
+    with open(path / dirname / (n + ".bpseq"), "w") as f_out:
+        for i, p in enumerate(pairs):
+            nuc = se[i]
+            if nuc not in ["A", "U", "C", "G"]:
+                nuc = np.random.choice(["A", "U", "C", "G"])
+            f_out.write(f"{i+1} {se[i]} {p}\n")
